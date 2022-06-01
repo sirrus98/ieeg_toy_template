98e5f49090b4221038592216f057d84b231653a6
sirrus98 <1766690896@qq.com>
2022-06-01 16:10:41 -0400

    eeg viewer

--- code/tools/plot_iEEG_data.py
+++ code/tools/plot_iEEG_data.py
@@ -23,30 +23,19 @@ def plot_iEEG_data(data, t, linecolor='k'):
         Returns figure handle
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     """
-    h, w = 10, 15
-
-
-    fig, ax = plt.subplots()
-
-    # fig.set_size_inches(15, 10)
-
-    # Show only bottom and left axis for visibility, format tick labels
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.ticklabel_format(useOffset=False)
-
     n_rows = data.shape[1]
+    h,w =  n_rows*1.5, 15
+    plt.figure(figsize=(w, h))
+
+    ax = plt.axes()
+
+
 
     ticklocs = []
-    ax.set_xlim(t[0], t[-1])
     dmin = data.min().min()
     dmax = data.max().max()
 
-    dr = (dmax - dmin) # Crowd them a bit.
-
-    y0 = dmin
-    y1 = (n_rows - 1) * dr + dmax
-    ax.set_ylim(y0, y1)
+    dr = (dmax - dmin)*0.7  # Crowd them a bit.
 
     segs = []
     for i in range(n_rows):
@@ -57,28 +46,18 @@ def plot_iEEG_data(data, t, linecolor='k'):
         else:
             print("Data is not in valid format")
 
-    for i in range(n_rows):
-        ticklocs.append(i * dr)
+    offsets = (np.arange(n_rows)*dr)[::-1]
 
-    offsets = np.zeros((n_rows, 2), dtype=float)
-    offsets[:, 1] = ticklocs
+    lines = np.array(segs)
 
+    pltlines = lines[:, :, 1] + offsets[:, np.newaxis]
 
-
-
-
-    colors = [mcolors.to_rgba(c)
-              for c in plt.rcParams['axes.prop_cycle'].by_key()['color']]
-
-    lines = LineCollection(segs, offsets=offsets/fig.dpi, transOffset=None, colors='k', linewidth=0.2)
-    ax.add_collection(lines)
+    plt.plot(lines[:, :, 0].T, pltlines.T, 'k', linewidth = 0.2)
 
     # # Set the yticks to use axes coordinates on the y axis
-    ax.set_yticks(ticklocs)
+    plt.yticks(offsets)
     if isinstance(data, pd.DataFrame):
         ax.set_yticklabels(data.columns)
-    ax.set_xlabel('Time (s)')
+    plt.xlabel('Time (s)')
 
-    # ax.set_title(iEEG_filename)
-    # fig.show()
-    return fig, ax
+    plt.show()
